a56f5232343e48221a460639158ed961137931fe
Sita <ariantishita@gmail.com>
2025-03-24 21:41:30 +0700

    Add files via upload

---
+++ TugasModul6_DBMS_2410506014.sql
@@ -0,0 +1,193 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 24, 2025 at 12:09 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `akademik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ambil_mk`
+--
+
+CREATE TABLE `ambil_mk` (
+  `nim` int(11) NOT NULL,
+  `kd_mk` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `ambil_mk`
+--
+
+INSERT INTO `ambil_mk` (`nim`, `kd_mk`) VALUES
+(101, 'PTI447'),
+(103, 'TIK333'),
+(104, 'PTI333'),
+(104, 'PTI777'),
+(105, 'PTI123'),
+(107, 'PTI777');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dosen`
+--
+
+CREATE TABLE `dosen` (
+  `kode_dos` int(11) NOT NULL,
+  `nama_dos` varchar(50) DEFAULT NULL,
+  `alamat_dos` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `dosen`
+--
+
+INSERT INTO `dosen` (`kode_dos`, `nama_dos`, `alamat_dos`) VALUES
+(10, 'Suharto', 'Jl. Jombang'),
+(11, 'Martono', 'Jl. Kalpataru'),
+(12, 'Rahmawati', 'Jl. Jakarta'),
+(13, 'Bambang', 'Jl. Bandung'),
+(14, 'Nurul', 'Jl. Raya Tidar');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jurusan`
+--
+
+CREATE TABLE `jurusan` (
+  `kode_jur` varchar(5) NOT NULL,
+  `nama_jur` varchar(100) DEFAULT NULL,
+  `kode_dos` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `jurusan`
+--
+
+INSERT INTO `jurusan` (`kode_jur`, `nama_jur`, `kode_dos`) VALUES
+('TE', 'Teknik Elektro', 10),
+('TM', 'Teknik Mesin', 13),
+('TS', 'Teknik Sipil', 23);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` int(11) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `jk` char(1) DEFAULT NULL,
+  `alamat` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama`, `jk`, `alamat`) VALUES
+(101, 'Arif', 'L', 'Jl. Kenangan'),
+(102, 'Budi', 'L', 'Jl. Jombang'),
+(103, 'Wati', 'P', 'Jl. Surabaya'),
+(104, 'Ika', 'P', 'Jl. Jombang'),
+(105, 'Tono', 'L', 'Jl. Jakarta'),
+(106, 'Iwan', 'L', 'Jl. Bandung'),
+(107, 'Sari', 'P', 'Jl. Malang');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matakuliah`
+--
+
+CREATE TABLE `matakuliah` (
+  `kd_mk` varchar(10) NOT NULL,
+  `nama_mk` varchar(100) DEFAULT NULL,
+  `sks` int(11) DEFAULT NULL,
+  `semester` int(11) DEFAULT NULL,
+  `kode_dos` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `matakuliah`
+--
+
+INSERT INTO `matakuliah` (`kd_mk`, `nama_mk`, `sks`, `semester`, `kode_dos`) VALUES
+('PTI123', 'Grafika Multimedia', 3, 5, 12),
+('PTI333', 'Basis DataTerdistribusi', 3, 5, 10),
+('PTI447', 'Praktikum Basis Data', 1, 3, 11),
+('PTI777', 'Sistem Informasi', 2, 3, 99),
+('TIK123', 'Jaringan Komputer', 2, 5, 33),
+('TIK333', 'Sistem Operasi', 3, 5, 10),
+('TIK342', 'Praktikum Basis Data', 1, 3, 11);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ambil_mk`
+--
+ALTER TABLE `ambil_mk`
+  ADD PRIMARY KEY (`nim`,`kd_mk`),
+  ADD KEY `kd_mk` (`kd_mk`);
+
+--
+-- Indexes for table `dosen`
+--
+ALTER TABLE `dosen`
+  ADD PRIMARY KEY (`kode_dos`);
+
+--
+-- Indexes for table `jurusan`
+--
+ALTER TABLE `jurusan`
+  ADD PRIMARY KEY (`kode_jur`);
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+
+--
+-- Indexes for table `matakuliah`
+--
+ALTER TABLE `matakuliah`
+  ADD PRIMARY KEY (`kd_mk`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `ambil_mk`
+--
+ALTER TABLE `ambil_mk`
+  ADD CONSTRAINT `ambil_mk_ibfk_1` FOREIGN KEY (`nim`) REFERENCES `mahasiswa` (`nim`),
+  ADD CONSTRAINT `ambil_mk_ibfk_2` FOREIGN KEY (`kd_mk`) REFERENCES `matakuliah` (`kd_mk`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
